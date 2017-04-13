d063a790c6106dc325cdad73e3d20ef72a2ff3ee
erinworth <eeworth@gmail.com>
2017-04-13 09:25:22 -0700

    Assignment 10 Collections: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,13 @@
 var pointsArray = document.getElementsByClassName('point');
 
-
-var revealPoint = function (point) {
+var revealPoint = function(point) {
 		point.style.opacity = 1;
 		point.style.transform = "scaleX(1) translateY(0)";
 		point.style.msTransform = "scaleX(1) translateY(0)";
 		point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-var animatePoints = function (points) {
+var animatePoints = function(points) {
     forEach(points, revealPoint);
 };
 
@@ -17,8 +16,6 @@ window.onload = function() {
 	if (window.innerHeight > 950) {
 		animatePoints(pointsArray);
 	}
-	var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
 	window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop ||
